b05b6f5bb6cc1e1db1afc6686808fadb6713e674
¨nitheesh-chapala¨ <nitheesh@contlo.com>
2023-07-11 19:03:57 +0530

    added notion tool

--- superagi/helper/prompt_reader.py
+++ superagi/helper/prompt_reader.py
@@ -23,3 +23,13 @@ class PromptReader:
             print(e.__str__())
             raise e
         return file_content
+
+    @staticmethod
+    def read_tools_schema_description(current_file: str, file: str) -> str:
+        file_path = str(Path(current_file).resolve().parent) + "/description/" + file
+        try:
+            f = open(file_path, "r")
+            file_content = f.read()
+        except FileNotFoundError as e:
+            raise e     
+        return file_content
